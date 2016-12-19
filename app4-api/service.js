14b56fcb862f236e39d6b9c5482917faef979669
Jon Low <lowjonc@gmail.com>
2016-12-19 09:32:44 -0700

    a

--- app4-api/service.js
+++ app4-api/service.js
@@ -1,34 +1,33 @@
-angular.module('apiApp').service('lukeServiceWalker',function ($http, $q) {
-  var baseUrl = 'http://swapi.co/'
-  this.getLuke = function(){
-    var luke = {};
-    var defer =$q.defer();
-    $http({
-      method: 'GET',
-      url: baseUrl + 'api/people/1'
-    })
-    .then(function (response) {
-      luke.name = response.data.name;
-      luke.height = response.data.height;
+angular.module('apiApp').service('lukeServiceWalker', function($http, $q) {
+    var baseUrl = 'http://swapi.co/'
+    this.getLuke = function() {
+        var luke = {};
+        var defer = $q.defer();
+        $http({
+                method: 'GET',
+                url: baseUrl + 'api/people/1'
+            })
+            .then(function(response) {
+                luke.name = response.data.name;
+                luke.height = response.data.height;
 
-      return $http({
-          method: 'GET',
-          url: response.data.species
-        })
+                return $http({
+                    method: 'GET',
+                    url: response.data.species
+                })
 
 
-      }).then(function (res) {
-        luke.species = res.data.name;
-        console.log(luke)
+            }).then(function(res) {
+                luke.species = res.data.name;
+                console.log(luke)
 
-        defer.resolve(luke)
+                defer.resolve(luke)
 
-      })
-
-//some sfshdfkjshdfksjdh
+            })
 
 
 
-    return defer.promise;
-  }
+
+        return defer.promise;
+    }
 })
